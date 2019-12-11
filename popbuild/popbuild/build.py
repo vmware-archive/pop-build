2094394d95bf51daff515b6c0292d184b78a86db
Thomas S Hatch <thatch45@gmail.com>
2019-12-10 19:39:27 -0700

    Add initial builder for external binary builds

---
+++ popbuild/popbuild/build.py
@@ -0,0 +1,29 @@
+'''
+The build plugin is used to execute the build routines for non-python components
+'''
+# Import python libs
+import subprocess
+import tempfile
+
+
+def make(hub, bname):
+    opts = hub.popbuild.BUILDS[bname]
+    build = opts['build']
+    if not build:
+        return
+    bdir = tempfile.mkdtemp()
+    os.chdir(bdir)
+    for proj, conf in build.items():
+        if 'make' in conf:
+            for cmd in conf['make']:
+                subprocess.call(cmd, shell=True)
+        if 'src' in conf and 'dest' in conf:
+            srcs = conf['src']
+            dest = os.path.join(opts['venv_dir'], conf['dest'])
+            if not isinstance(src, list):
+                srcs = [srcs]
+            for src in srcs:
+                if os.path.isfile(src):
+                    shutil.copy(src, dest)
+                elif os.path.isdir(src):
+                    shutil.copytree(src, dest)
